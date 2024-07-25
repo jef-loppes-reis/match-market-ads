ffc2cda350587c5e1f08757755edac49d4119231
jef-loppes-reis <jefersondip25@gmail.com>
2024-07-25 09:51:55 -0300

    feat: progresso no metodo de uso

--- clonagem/uso.py
+++ clonagem/uso.py
@@ -1,30 +1,33 @@
 from json import load
 
+from aplicacao import Aplicacao
+from clone import Clanador
 from ecomm import MLInterface
 from httpx import Response
-from pandas import read_excel, read_feather, merge, DataFrame
 from rich import print as rprint
-from clone import Clanador
+from pandas import read_excel, read_feather, merge, DataFrame
+from tqdm import tqdm
 
 
 def get_number_photo(lista_name_photo: str) -> list[str]:
     return [name.split('_')[-1:][0].replace('.jpg', '') for name in lista_name_photo]
 
 if __name__ == '__main__':
-    HEADERS: dict = MLInterface(1)._headers()
+    ML_INTERFACE: MLInterface = MLInterface(1)
+    HEADERS: dict = ML_INTERFACE._headers()
     CAMINHO_FOTOS: str = '../photo_validation/out_files_photos/%s'
-    NOME_LOJA: str = 'sampel'
+    NOME_LOJA: str = 'monroe'
 
     with open('./data/sale_terms.json', 'r', encoding='utf-8') as fp:
         sale_terms = load(fp)
 
     df_photos: DataFrame = read_feather(
-        '../data/conferencia_fotos_sampel.feather')
+        f'../data/conferencia_fotos_{NOME_LOJA}.feather')
     df_photos = df_photos.query('~mlb.isna()').reset_index(drop=True).copy()
     df_photos['number_photo'] = get_number_photo(
         df_photos.path_file_photo.fillna('0'))
 
-    df_clona_genuino = read_excel('../data/sampel.xlsx', dtype=str)
+    df_clona_genuino = read_excel(f'../data/{NOME_LOJA}.xlsx', dtype=str)
     df_clona_genuino = df_clona_genuino.set_axis(
         df_clona_genuino.columns.str.lower(), axis=1).copy()
     df_clona_genuino.loc[:,
@@ -51,33 +54,46 @@ if __name__ == '__main__':
 
     clonagem.read_product_siac()
 
-    for mlb in df_clona_copy.mlb.unique():
-        row: DataFrame = df_clona_copy.query('mlb == @mlb')
+    # aplic: Aplicacao = Aplicacao()
+    # aplic.read_apl()
+    # aplic.criar_ano_inicial_ano_final()
+    # aplic.etl_tabela_aplic()
 
-        mlb_concorrente: str = row.loc[0, 'mlb']
-        cod_ref_ecomm: str = row['sku_certo'].unique()[0]
-        codpro: str = clonagem.convert_sku_to_codpro(sku=cod_ref_ecomm)[0]
+    for mlb in tqdm(df_clona_copy.mlb.unique(), desc='Anunciando...:', colour='blue'):
+        row: DataFrame = df_clona_copy.query('mlb == @mlb').reset_index(drop=True).copy()
+        sku: str = row['sku_certo'].unique()[0]
+        codpro: str = clonagem.convert_sku_to_codpro(
+            sku=sku
+        )[0]
+        # descricao: str = aplic.criar_descricao(
+        #     titulo='',
+        #     aplicacao_veiculos='',
+        #     marca=row.loc[0, 'marca'],
+        #     num_fab=row.loc[0, 'mpn'],
+        #     oems=row.loc[0, 'oem'],
+        #     multiplo_venda=1,
+        #     codpro=codpro
+        # )
+        print(row)
 
         clonagem.df_siac_filter(
             codpro_produto=codpro
         )
 
-        clonagem.mlb_infos(mlb_concorrente)
+        mlb_info: tuple[Response, Response] = clonagem.mlb_infos(
+            item_id_ml=row.loc[0, 'mlb']
+        )
 
         clonagem.gerar_payload_cadastro(
             list_path_img=[
-                CAMINHO_FOTOS % f'{NOME_LOJA}/{name_photo}' for name_photo in row['path_file_photo']],
-            sku=cod_ref_ecomm
+                CAMINHO_FOTOS % f'monroe/{name_photo}' for name_photo in row['path_file_photo']],
+            sku=sku
         )
 
-        cadastro: Response = clonagem.cadastro()
+        retorno_cadastro: str = ML_INTERFACE.post_item(clonagem.corpo_clonagem)
 
-        compati_clone: Response = clonagem.compatibilidades(
-            item_id_ml_clone=mlb_concorrente,
-            item_id_novo=cadastro.get('id'),
-            view_compati=False
-        )
+        clonagem.compatibilidades()
 
+        rprint(clonagem.corpo_clonagem)
+        rprint(retorno_cadastro)
         break
-
-    rprint(corpo_cadastro)
